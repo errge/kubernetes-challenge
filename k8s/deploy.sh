8e7f324cdfda5427b0dd08a914e23f35d3bf6f01
Gergely Risko <errge@nilcons.com>
2019-07-16 14:16:50 +0200

    Fourth step: using a ConfigMap, to make the name configurable

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -4,7 +4,8 @@ set -e
 
 kubectl apply -f namespace.yaml
 kubectl apply -f service.yaml
+kubectl apply -f configmap.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f ingress.yaml
 
-echo 'Use "kubectl -n challenge get ing" to monitor when your ingress becomes ready'
+echo 'Use "kubectl -n challenge get pods" to monitor the new config becoming ready'
