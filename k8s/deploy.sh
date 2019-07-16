0b06d999a9533f7ec0c4b75842b46c15bc97d2db
Gergely Risko <errge@nilcons.com>
2019-07-16 13:03:49 +0200

    Second step: service that directly exposes the TCP port

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -3,12 +3,7 @@
 set -e
 
 kubectl apply -f namespace.yaml
+kubectl apply -f service.yaml
 kubectl apply -f deployment.yaml
 
-# port 4000/tcp is reserved for NX on my desktop :(
-kubectl -n challenge port-forward deployment/hello 8888:4000
-
-# other idea to debug would be:
-#   - kubectl -n challenge get pod -o wide   <- get the podip from here
-#   - kubectl -n challenge run -it debian --image=debian --restart=Never --rm
-#   - apt update && apt install telnet && telnet <podip> 4000
+echo 'Use "kubectl -n challenge get svc" to monitor when your load balancer becomes ready'
